a36f571e5cdb4ecc59b0a218316180fff52b68f1
psudhakarsudhap <psudhakarsudhap@gmail.com>
2020-10-18 05:38:35 +0530

    Updates

---
+++ precache-manifest.41abc8d9bef8cc77925e1cec25fdc210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f122a231c5eb4e3f9ae5fc20e7c337",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "046eb305652691c320ed",
+    "url": "/test/static/css/main.1a9f2c4a.chunk.css"
+  },
+  {
+    "revision": "fe825fbcf69b95f18681",
+    "url": "/test/static/js/2.ee950989.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/test/static/js/2.ee950989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046eb305652691c320ed",
+    "url": "/test/static/js/main.a92d63b2.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
